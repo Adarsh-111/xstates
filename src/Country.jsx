ed6bffee934b11e8defd84479cae921d4efc0ca6
Adarsh Shah <sahadarsh@gmail.com>
2026-02-09 18:01:56 +0530

    Change commit

--- src/Country.jsx
+++ src/Country.jsx
@@ -6,8 +6,19 @@ function Country({ selectedCountry, setSelectedCountry }) {
   // Fetch countries when component loads
   useEffect(() => {
     fetch("https://location-selector.labs.crio.do/countries")
-      .then((res) => res.json())
-      .then((data) => setCountries(data));
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Country API failed");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setCountries(Array.isArray(data) ? data : []);
+      })
+      .catch(() => {
+        // Graceful fallback for API failure (required by Cypress)
+        setCountries([]);
+      });
   }, []);
 
   return (
